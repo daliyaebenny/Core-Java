5004ae088123a0e5b0583194a7ffe0728bd8c3ad
Daliya Benny <daliyaebenny@gmail.com>
2022-06-12 22:16:18 -0700

    Refactor

---
+++ Activities/test/MovieTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class MovieTest {
+
+
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
